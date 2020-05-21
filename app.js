ed72a01a9000479c4cfcb96660e8b948af54b633
ShujoyI <shujoyi@gmail.com>
2020-05-20 23:44:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var coolRouter = require('./routes/users/cool');
 
 var app = express();
 
