cda4f19319c94503cc3021eea87eab63d8f16738
ShujoyI <shujoyi@gmail.com>
2020-05-21 00:11:06 -0700

    Updated users.js
    Added cool route handler to users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users/cool listing. */
+router.get('/', function(req, res, next) {
+  res.send('It\'s so cool!');
+});
 module.exports = router;
