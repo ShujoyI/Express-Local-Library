f7a837bccd04d643a7ba095a09c5a50a63d3f7e7
ShujoyI <shujoyi@gmail.com>
2020-05-28 15:17:14 -0700

    Update genre.js
    Added virtual for genre's URL

--- models/genre.js
+++ models/genre.js
@@ -8,7 +8,12 @@ var GenreSchema = new Schema(
   }
 );
 
-
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
 
 //Export model
 module.exports = mongoose.model('Genre', GenreSchema);
