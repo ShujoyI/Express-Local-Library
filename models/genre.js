1441d83a62dc6e398b482fb771f855773a340e8b
ShujoyI <shujoyi@gmail.com>
2020-05-28 15:16:56 -0700

    Update genre.js
    Started schema for Genre database model

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, max: 100},
+  }
+);
+
+
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
