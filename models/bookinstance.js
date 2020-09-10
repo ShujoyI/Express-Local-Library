3608b440089432d2dd3f5f15f46637411b5a36b1
ShujoyI <shujoyi@gmail.com>
2020-09-10 13:21:25 -0700

    Update bookinstance.js
    Fixed bug

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -17,7 +17,9 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
-})
+});
+
+BookInstanceSchema
 .virtual('due_back_formatted')
 .get(function () {
   return moment(this.due_back).format('MMMM Do, YYYY');
