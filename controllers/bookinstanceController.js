0ff29dc24aa4e52e7c8fd3d5076ddef0a40b9ae8
ShujoyI <shujoyi@gmail.com>
2020-09-10 12:38:42 -0700

    Update bookinstanceController.js
    Get list of all book instances, populate with aSsociated book information, then pass the list to template

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,8 +1,17 @@
 var BookInstance = require('../models/bookinstance');
+const { nextTick } = require('async');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list');
+    
+    BookInstance.find()
+    .populate('book')
+    .exec(function (err, list_bookinstances) {
+        if (err) {return next(err); }
+        // Successful, so render data
+        res.render('bookinstance_list', 
+        {title: 'Book Instance List', bookinstance_list: list_bookinstances});
+    });
 };
 
 // Display detail page for a specific BookInstance.
