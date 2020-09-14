3186d920205714a46aa6732b21b3bbce1f15e630
ShujoyI <shujoyi@gmail.com>
2020-09-14 00:29:25 -0700

    Update genreController.js
    Fixed typo in import statement

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,6 @@
 var Genre = require('../models/genre');
 var Book = require('../models/book');
-var async = requre('async');
+var async = require('async');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
